1cd5a6a7c13154b79081c213bca7a7b7ba0cf30a
ahmed atef <aatef@192.168.1.6>
2020-02-04 00:41:24 +0200

    fixing some issuesf

--- Mainmenu.sh
+++ Mainmenu.sh
@@ -1,56 +1,64 @@
 dbsScreen=true;
 
-	################################################################################
-	# Databases Screen
-	while $dbsScreen; do
-	PS3="dbscreen>"
-		clear;
-		. ./MakeDB.sh
-		. ./ShowDBS.sh;
-				separator;
-		echo -e "\t\tYour Existing Databases:\n$(find -maxdepth 1 -type d | cut -d'/' -f2 | sed '1d')"
-		separator;
-		select choice in "Create a new database" "Use existing Database" "Drop Database" "Back" "Show dbs"; do 
-		case $REPLY in
-			1 ) #Create a database
-				separator;
-				createDb;
-
-					echo -e "\e[42mDatabase is loading...\e[0m"
-				echo press any key
-				read
-				;;
-			2 ) # Use existing
-				# separator;
-				# useExistingDb;
-				;;
-			3 ) # Drop Database
-				# separator;
-				# dropDb;
-				;;
-			4 ) # Back
-break;;			
-				5) #showDatabases
-				
-			./ShowDBS.sh;
-	 	echo -e "\e[42mDatabase is loading...\e[0m"
-				echo press any key
-				read
-
-	
-
-			
-				;;
-			* )
-				echo -e "\e[41minvalid entry\e[0m"
-				echo press any key
-				read
-				;;
-		esac
-		break
-		done
-	done
-	
 ################################################################################
+# Databases Screen
+while $dbsScreen; do
+    PS3="dbscreen>"
+    clear;
+    . ./MakeDB.sh
+    . ./ShowDBS.sh;
+    . ./DropDatabases.sh
+    separator;
+    echo -e "\t\tYour Existing Databases:\n$(find -maxdepth 1 -type d | cut -d'/' -f2 | sed '1d')"
+    separator;
+    select choice in "Create a new database" "Use existing Database" "Drop Database" "Exit" "Show dbs"; do
+        case $REPLY in
+            1 ) #Create a database
+                separator;
+                createDb;
+                
+                echo -e "\e[42mDatabase is loading...\e[0m"
+                echo press any key
+                read
+            ;;
+            2 ) # Use existing
+                # separator;
+                # useExistingDb;
+            ;;
+            3 ) # Drop Database
+                # read
+                separator;
+                # echo "DROP"
+                dropDb
+                
+            ;;
+            
+            
+            4 ) # Exit
+                  exit
+break;;
+            5) #showDatabases
+                
+                ShowDBS;
+                echo -e "\e[42mDatabase is loading...\e[0m"
+                echo press any key
+                read
+                
+                
+                
+                
+            ;;
+            * )
+                echo -e "\e[41minvalid entry\e[0m"
+                echo press any key
+                read
+            ;;
+        esac
+        break
+    done
+done
+
+################################################################################
+
 
 
