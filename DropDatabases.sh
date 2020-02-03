133ab712fbec9e302d189422ff5cc4bd0d2405ff
ahmed atef <aatef@192.168.1.6>
2020-02-04 00:18:27 +0200

    Drop Database

---
+++ DropDatabases.sh
@@ -0,0 +1,26 @@
+
+
+function dropDb {
+    separator;
+    echo enter the name of the database
+    read db
+    db="$db"
+    
+    if [[ "Databases/$db" = '' ]]; then
+        echo -e "\e[41minvalid entry, please enter a correct name\e[0m"
+        echo press any key
+        read
+        
+        elif ! [[ -d Databases/"$db" ]]; then
+        echo -e "\e[41mthis database doesn't exist\e[0m"
+        echo press any key
+        read
+        #finally remove Database
+    else
+        echo $db;
+        rm -r  Databases/$db
+        echo -e "\e[42m$db removed from your databases\e[0m"
+        echo press any key
+        read
+    fi
+}
