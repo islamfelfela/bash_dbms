9018f85558c88de8ef39052ab06e84e1380a5d58
ahmedatef00 <ahmed62437@student.must.edu.eg>
2020-02-08 00:45:20 +0200

    Drop Table

--- operations.sh
+++ operations.sh
@@ -1,57 +1,74 @@
 #! /bin/bash
 
-. ./insertIntoTable.sh
+# Table Menue
 operations(){
     echo "Operations on table"
     while true
-    do 
-        select ch in "creat table" "list tables" "insert into table" "select all from table" "select row" "back";
     do
-        case $ch in
-            "creat table")
-                echo "creat table"
-                clear
-                creatTable
-                break
+        select ch in "creat table" "list tables" "insert into table" "select all from table" "select row" "delete row" "delete table" "back";
+        do
+            case $ch in
+                "creat table")
+                    echo "creat table"
+                    clear
+                    creatTable
+                    break
                 ;;
-            "list tables" )
-                echo "list tables"
-                clear
-                listeTables
-                break
+                "list tables" )
+                    echo "list tables"
+                    clear
+                    listTables
+                    break
                 ;;
-            "insert into table")
-                echo "insert into table"
-                clear
-                insertData
-                break
+                "insert into table")
+                    echo "insert into table"
+                    clear
+                    insertData
+                    break
                 ;;
-            "select all from table")
-                echo "select all"
-                clear
-                selectAllFromTable
-                break
+                "select all from table")
+                    echo "select all"
+                    clear
+                    selectAllFromTable
+                    break
                 ;;
-            
-            "select row")
-                echo "select row"
-                clear
-                selectRecord
-                break
+                
+                "select row")
+                    echo "select row"
+                    clear
+                    displayRow
+                    break
                 ;;
-            "back")
-                echo "back"
-                clear
-                Mainmenu
-                break
+                "delete row")
+                    echo "delete row"
+                    clear
+                    deleteRow
+                    break
                 ;;
-            *)
-                echo "Wrong Input WWWWWW"
+                "delete table")
+                    echo "delete table"
+                    clear
+                    deleteTable
+                    break
                 ;;
-
-        esac
+                
+                "back")
+                    echo "back"
+                    clear
+                    echo "back"
+                    cd ..
+                    cd ..
+                    Mainmenu
+                    
+                    break
+                ;;
+                *)
+                    echo "Wrong Input plz try again later"
+                ;;
+                
+            esac
+        done
+        operations
     done
-    operations
-     done
     
 }
